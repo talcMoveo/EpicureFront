ee9bbcf1b41e750067b4d0d035872ff7ac5bd2e3
Tal Cohen <talc@moveo.co.il>
2022-07-21 12:50:42 +0300

    midway commit

--- src/components/Homepage/ChefOfTheWeek.tsx
+++ src/components/Homepage/ChefOfTheWeek.tsx
@@ -34,7 +34,7 @@ const ChefOfTheWeek: React.FC<props> = (props) => {
             {
             breakpoint: 1024,
             settings: {
-                slidesToShow: 1.2,
+                slidesToShow: 3,
                 slidesToScroll: 1,
                 infinite: true,
                 arrows: false
